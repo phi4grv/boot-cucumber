68e098181e17253519682fab99190bc28a9bb861
Hunter Stern <hunter.stern@fundingcircle.com>
2017-05-01 17:08:03 -0700

    Run tests in a pod

--- build.boot
+++ build.boot
@@ -1,9 +1,14 @@
-(set-env!
+(set-env!  
   :resource-paths #{"src"}
-  :source-paths #{"test"}
-  :dependencies   '[[info.cukes/cucumber-clojure "1.2.5"]]
+  :source-paths #{"src"}
+  :dependencies   '[[info.cukes/cucumber-clojure "1.2.5"]
+                    [com.cemerick/pomegranate "0.3.1"]
+                    #_[adzerk/boot-test "1.2.0" :scope "test"]]
   :repositories [["maven" {:url "http://central.maven.org/maven2/"}]
                  ["clojars" {:url "https://clojars.org/repo/"}]])
+
+#_(require 
+         '[adzerk.boot-test :refer :all :rename {test test*}])
 
 (def +version+ "1.0.0-SNAPSHOT")
 
@@ -15,7 +20,13 @@
        :scm         {:url "https://github.com/vonrosen/boot-cucumber"}
        :license     {"EPL" "http://www.eclipse.org/legal/epl-v10.html"}})
 
-(ns-unmap *ns* 'test)
+#_(deftask test
+   "Override boot-test/test to get necessary env vars prior to running the tests"
+   []
+   (comp   
+    (test*)))
+
+;(ns-unmap *ns* 'test)
 
 (deftask build []
   (comp
--- src/vonrosen/boot_cucumber.clj
+++ src/vonrosen/boot_cucumber.clj
@@ -1,9 +1,23 @@
 (ns vonrosen.boot-cucumber
-  (:require   [boot.core :as core])  
-  (:import [cucumber.api.cli Main]))
+  {:boot/export-tasks true}
+  (:require [boot.pod  :as pod]
+            ;[boot.task.built-in :refer [target]]
+            [boot.core :as core]
+            [cemerick.pomegranate :as pom]
+            [clojure.test :refer :all]
+            ;[adzerk.boot-test :refer [test] :rename {test test*}]
+            ;[clojure.java.io :as io]
+            ;[clojure.tools.namespace.find :refer [find-namespaces-in-dir]]
+            )
+  (:import [cucumber.api.cli Main])
+  #_(:use clojure.test))
+
+;(load-data-readers!)
+
+(def task-args (atom nil))
 
 (core/deftask cukes
-  ""
+  "Run cucumber tests."
   [g glue VALUE str "Where glue code (step definitions, hooks and plugins) are loaded from." 
    p plugin VALUE str "Register a plugin. Built-in formatter PLUGIN types: junit,
                                          html, pretty, progress, json, usage, rerun,
@@ -19,24 +33,59 @@
    s strict bool "Treat undefined and pending steps as errors."
    v version bool "Print version."
    i i18n VALUE str " List keywords for in a particular language. Run with \"--i18n help\" to see all languages"]
-  (let [task-args (conj []
-                        (if glue
-                          (str "--glue" " " glue)
-                          #_(str "--glue" " " "features/step_definitions" " " "features"))
-                        (if plugin
-                          (str "--plugin" " " plugin))
-                        (if tags
-                          (str "--tags" " " tags))
-                        (if name
-                          (str "--name" " " name))
-                        (if dry-run
-                          "-d")
-                        (if monochrome
-                          "-m")
-                        (if strict
-                          "-s")
-                        (if version
-                          "--version")
-                        (if i18n
-                          (str "--i18n" " " i18n)))]
-    (. cucumber.api.cli.Main (main (into-array task-args)))))
+  (let [args (vec (flatten 
+                     (remove nil? (conj [] 
+                                        (if glue
+                                          ["--glue" glue]
+                                          ["--glue" "features"])
+                                        (if plugin
+                                          ["--plugin" plugin])
+                                        (if tags
+                                          ["--tags" tags])
+                                        (if name
+                                          ["--name" name])
+                                        (if dry-run
+                                          ["-d"])
+                                        (if monochrome
+                                          ["-m"])
+                                        (if strict
+                                          ["-s"])
+                                        (if version
+                                          ["--version"])
+                                        (if i18n
+                                          ["--i18n" i18n])
+                                        ["features"]))))]
+    (core/with-pre-wrap fileset
+          #_(prn ~@(->> fileset
+                                     core/input-dirs
+                                     (map (memfn getPath))))
+          #_(prn        
+             (pod/with-eval-in (worker-pods :refresh)
+               (distinct (mapcat #(find-namespaces-in-dir (io/file %)) 
+                                 ~@(->> fileset
+                                     core/input-dirs
+                                     (map (memfn getPath)))))
+              
+               #_(all-ns* ~@(->> fileset
+                                                             core/input-dirs
+                                                             (map (memfn getPath))))))
+          (reset! task-args args)
+          ;(prn @task-args)          
+          ;(test* :include #".*vonrosen.*$")
+          ;(core/add-source fileset (io/file "features/step_definitions"))
+          ;(pom/add-classpath "features")          
+          (pom/add-classpath "features/step_definitions")
+          (run-tests (symbol "vonrosen.boot-cucumber"))                    
+          (core/commit! fileset)
+    
+        ;(test* :include #".*ui-tests.*$" :startup '#{vonrosen.boot-cucumber/cucumber-tests})    
+        #_(run-tests (symbol "vonrosen.boot-cucumber"))    
+        #_(test* :include #"^.*src.*$")    
+        #_(test* :namespaces #{(symbol "vonrosen.boot-cucumber")})
+        #_(test* :include #".*ui-tests.*$"))))
+
+(deftest cucumber-tests  
+  (prn @task-args)
+  ;(core/merge-env! :source-paths #{"ui_tests/clj" "features"})
+  ;(core/merge-env! :resource-paths #{"features"})  
+  (. cucumber.api.cli.Main (main (into-array @task-args))))
